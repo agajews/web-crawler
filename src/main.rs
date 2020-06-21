b77f775ba490e1e26b352007151cd70f45c4b36a
Alex Gajewski <agajews@gmail.com>
2020-06-21 16:20:02 -0400

    yielding more

--- src/main.rs
+++ src/main.rs
@@ -204,16 +204,12 @@ fn find_task<T>(
     local.pop().or_else(|| {
         // println!("waiting for work...");
         // Otherwise, we need to look for a task elsewhere.
-        iter::repeat_with(|| {
-            // Try stealing a batch of tasks from the global queue.
-            global.steal_batch_and_pop(local)
-                // Or try stealing a task from one of the other threads.
-                .or_else(|| stealers.iter().map(|s| s.steal()).collect())
-        })
+        // Try stealing a batch of tasks from the global queue.
+        global.steal_batch_and_pop(local)
+            // Or try stealing a task from one of the other threads.
+            .or_else(|| stealers.iter().map(|s| s.steal()).collect())
+            .success()
         // Loop while no task was stolen and any steal operation needs to be retried.
-        .find(|s| !s.is_retry())
-        // Extract the stolen task, if there is one.
-        .and_then(|s| s.success())
     })
     // local.pop().or_else(|| global.steal_batch_and_pop(local).success())
 }
