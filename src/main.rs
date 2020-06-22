b2a819583d3f6a727752c8d086441f9275ce89b2
Alex Gajewski <agajews@gmail.com>
2020-06-21 20:42:06 -0400

    full scraping

--- src/main.rs
+++ src/main.rs
@@ -43,8 +43,7 @@ const BLOOM_BYTES: usize = 10_000_000_000;
 const EST_URLS: usize = 1_000_000_000;
 
 lazy_static! {
-    // static ref ACADEMIC_RE: Regex = Regex::new(r"^.+\.(edu|ac\.??)$").unwrap();
-    static ref ACADEMIC_RE: Regex = Regex::new(r"columbia\.edu").unwrap();
+    static ref ACADEMIC_RE: Regex = Regex::new(r"^.+\.(edu|ac\.??)$").unwrap();
 }
 
 fn is_academic(url: &Url) -> bool {
