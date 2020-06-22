170c4db6700423a65b71cef397ec6d1f77162bb0
Alex Gajewski <agajews@gmail.com>
2020-06-22 01:16:01 -0400

    tuning tcp

--- setup.sh
+++ setup.sh
@@ -8,5 +8,16 @@ sudo su -c 'echo "	*	soft	nofile	200000" >> /etc/security/limits.conf'
 sudo su -c 'echo "	*	hard	nofile	200000" >> /etc/security/limits.conf'
 sudo su -c "sed -i 's/4096/unlimited/g' /etc/security/limits.d/20-nproc.conf"
 
+sudo su -c "echo 'net.core.wmem_max=12582912' >> /etc/sysctl.conf"
+sudo su -c "echo 'net.core.rmem_max=12582912' >> /etc/sysctl.conf"
+sudo su -c "echo 'net.ipv4.tcp_rmem= 10240 87380 12582912' >> /etc/sysctl.conf"
+sudo su -c "echo 'net.ipv4.tcp_wmem= 10240 87380 12582912' >> /etc/sysctl.conf"
+sudo su -c "echo 'net.ipv4.tcp_window_scaling = 1' >> /etc/sysctl.conf"
+sudo su -c "echo 'net.ipv4.tcp_timestamps = 1' >> /etc/sysctl.conf"
+sudo su -c "echo 'net.ipv4.tcp_sack = 1' >> /etc/sysctl.conf"
+sudo su -c "echo 'net.ipv4.tcp_no_metrics_save = 1' >> /etc/sysctl.conf"
+sudo su -c "echo 'net.core.netdev_max_backlog = 5000' >> /etc/sysctl.conf"
+sudo sysctl -p
+
 mkdir /tmp/crawler-meta /tmp/crawler-index
 export META_DIR=/tmp/crawler-meta INDEX_DIR=/tmp/crawler-index
