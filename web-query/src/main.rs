7384f12d40de46578c2752aa5ed5de996955902c
Alex Gajewski <agajews@gmail.com>
2020-07-14 15:42:32 -0400

    less expressive again

--- web-query/src/main.rs
+++ web-query/src/main.rs
@@ -33,7 +33,7 @@ fn get_scores(shard: &mut IndexShard, terms: &[String]) -> Option<Vec<u8>> {
         postings.push(shard.get_postings(term, SHARD_SIZE)?);
     }
     let mut idfs = postings.iter()
-        .map(|posting| (posting.iter().filter(|byte| **byte > 0).count() * (1 << 12)) / SHARD_SIZE)
+        .map(|posting| (posting.iter().filter(|byte| **byte > 0).count() * (1 << 10)) / SHARD_SIZE)
         .map(|idf| 16 - (idf as u16).leading_zeros() as u8 + 1)
         .map(|log_idf| log_idf * 4) // scales between 0 and 64
         .collect::<Vec<_>>();
