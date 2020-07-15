c8f2e97013ffed074775195ba2cf609049db37e4
Alex Gajewski <agajews@gmail.com>
2020-07-15 16:10:21 -0400

    logging seg len

--- web-index/src/lib.rs
+++ web-index/src/lib.rs
@@ -126,14 +126,11 @@ impl RunEncoder {
                 }
                 i += 1;
             } else {
+                println!("deserializing seg of len {}", seg_len);
                 if i + seg_len > serialized.len() {
                     return None;
                 }
                 decoded[k..(k + seg_len)].copy_from_slice(&serialized[i..(i + seg_len)]);
-                // for j in 0..(seg_len as usize) {
-                //     decoded[k] = *serialized.get(i + j)?;
-                //     k += 1;
-                // }
                 i += seg_len;
                 k += seg_len;
             }
