ed4169df35dcbd0958d12249bbb2c282b296cedb
Alex Gajewski <agajews@gmail.com>
2020-07-15 14:01:13 -0400

    checking count_nonzero correctness

--- web-query/src/main.rs
+++ web-query/src/main.rs
@@ -46,11 +46,12 @@ fn count_nonzero(slice: &[u8]) -> u32 {
 fn compute_idfs(postings: &Vec<Vec<u8>>) -> Vec<u8> {
     let mut idfs = Vec::with_capacity(postings.len());
     for posting in postings {
-        let mut total_count: u32 = 0;
-        for i in (0..(posting.len())).step_by(32 * 128) {
-            let end = std::cmp::min(i + 32 * 128, posting.len());
-            total_count += count_nonzero(&posting[i..end]);
-        }
+        // let mut total_count: u32 = 0;
+        // for i in (0..(posting.len())).step_by(32 * 128) {
+        //     let end = std::cmp::min(i + 32 * 128, posting.len());
+        //     total_count += count_nonzero(&posting[i..end]);
+        // }
+        let total_count = posting.iter().filter(|byte| **byte > 0).count() as u32;
         let normalized_count = total_count * (1 << 16) / SHARD_SIZE as u32;
         let log_idf = 32 - normalized_count.leading_zeros();
         let idf = 1 << (log_idf / 2);
