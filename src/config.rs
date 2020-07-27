d8cf5e9642c92e136bce04c7c4c06f6686634110
Alex Gajewski <agajews@gmail.com>
2020-07-27 13:18:34 -0400

    no scheduler sleeping

--- src/config.rs
+++ src/config.rs
@@ -43,7 +43,7 @@ impl Config {
                 12_500_000
                 // 1000
             },
-            scheduler_sleep: Duration::from_millis(1),
+            scheduler_sleep: Duration::from_millis(0),
             locality_clear_prob: 0.01,
             work_queue_cap: 100,
             min_run_len: 32,
