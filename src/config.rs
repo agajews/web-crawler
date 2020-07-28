34b5105c47260ba3cc6151020f941a5d8bbe49c0
Alex Gajewski <agajews@gmail.com>
2020-07-27 21:26:54 -0400

    back to the bigger cap again as an experiment

--- src/config.rs
+++ src/config.rs
@@ -39,7 +39,7 @@ impl Config {
             page_capacity: 60,
             max_url_len: 250,
             page_size_bytes: 4096 * 4,
-            scheduler_queue_cap: 100,
+            scheduler_queue_cap: 10_000,
             n_pqueue_threads: if debug { 2 } else { 32 },
             pqueue_cache_cap: if debug { 20 } else {
                 12_500_000
