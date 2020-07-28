b7f34e3d6505a649d358020ffe8d9fcb788d9711
Alex Gajewski <agajews@gmail.com>
2020-07-28 18:46:56 -0400

    bigger scheduler queue cap

--- src/config.rs
+++ src/config.rs
@@ -39,8 +39,8 @@ impl Config {
             page_capacity: 60,
             max_url_len: 250,
             page_size_bytes: 4096 * 4,
-            scheduler_queue_cap: 10,
-            work_queue_cap: 10,
+            scheduler_queue_cap: 100,
+            work_queue_cap: 100,
             n_pqueue_threads: if debug { 2 } else { 32 },
             pqueue_cache_cap: if debug { 20 } else {
                 12_500_000
