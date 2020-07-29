d768286b5b555fe3a5c5a73c55bcb3f2c5e5b40f
Alex Gajewski <agajews@gmail.com>
2020-07-29 15:40:35 -0400

    bigger queues for better locality?

--- src/config.rs
+++ src/config.rs
@@ -42,7 +42,7 @@ impl Config {
             max_url_len: 250,
             page_size_bytes: 4096 * 4,
             scheduler_queue_cap: 1000,
-            work_queue_cap: 100,
+            work_queue_cap: 200,
             work_empty_threshold: 10,
             n_pqueue_threads: if debug { 2 } else { 32 },
             pqueue_cache_cap: if debug { 20 } else {
@@ -62,7 +62,7 @@ impl Config {
                 "https://cam.ac.uk",
             ],
             user_agent: String::from("Rustbot/0.4"),
-            n_threads: if debug { 1 } else { 75 },
+            n_threads: if debug { 1 } else { 100 },
             n_pqueues: if debug { 2 } else { 32 },
             min_n_tokens: 250,
         };
