1b30819bcf5166d96182d60bf5b3833df31770aa
Alex Gajewski <agajews@gmail.com>
2020-07-29 14:30:31 -0400

    not holding robots lock too much

--- src/robots.rs
+++ src/robots.rs
@@ -2,7 +2,7 @@ use crate::monitor::MonitorHandle;
 use crate::client::Client;
 use crate::config::Config;
 
-use tokio::sync::Mutex;
+use std::sync::Mutex;
 use regex::Regex;
 use std::collections::BTreeMap;
 use url::Url;
@@ -27,20 +27,19 @@ impl RobotsChecker {
 
     pub async fn allowed(&self, url: &Url, client: &mut Client) -> bool {
         let host = url.host_str().unwrap();
-        let mut cache = self.cache.lock().await;
-        self.monitor.inc_robots_queries();
-        let prefixes = match cache.get(host) {
-            Some(prefixes) => {
+        {
+            let cache = self.cache.lock().unwrap();
+            self.monitor.inc_robots_queries();
+            if let Some(prefixes) = cache.get(host) {
                 self.monitor.inc_robots_hits();
-                prefixes
-            },
-            None => {
-                let prefixes = self.fetch_robots(url, client).await;
-                cache.insert(String::from(host), prefixes);
-                &cache[host]
-            },
-        };
-        Self::match_url(url, prefixes)
+                return Self::match_url(url, prefixes);
+            }
+        }
+        let prefixes = self.fetch_robots(url, client).await;
+        let allowed = Self::match_url(url, &prefixes);
+        let mut cache = self.cache.lock().unwrap();
+        cache.insert(String::from(host), prefixes);
+        allowed
     }
 
     fn match_url(url: &Url, prefixes: &Option<Vec<String>>) -> bool {
